7523aefefa5bba9cacfb7e7dc9e78f3a2e262de0
Ahmadreza Mozaffary <79336004+AhmadrezaMozaffary@users.noreply.github.com>
2022-07-14 19:29:35 +0430

    Creating test suite

--- src/components/Greetings.test.js
+++ src/components/Greetings.test.js
@@ -1,9 +1,11 @@
 import { render, screen } from "@testing-library/react";
 import Greetings from "./Greetings";
 
-test("Hello World printed", () => {
-  render(<Greetings />);
+describe("-- Greeting : ", () => {
+  test("Hello World printed", () => {
+    render(<Greetings />);
 
-  const HelloWorldEl = screen.getByText(/Hello World/i);
-  expect(HelloWorldEl).toBeInTheDocument();
+    const HelloWorldEl = screen.getByText(/Hello World/i);
+    expect(HelloWorldEl).toBeInTheDocument();
+  });
 });
